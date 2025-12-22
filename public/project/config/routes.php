87693cdfc865d900db76f1470e51306f17ac723f
Fox Tail <foxtail25@gmail.com>
2025-12-22 18:02:38 +0300

    repit

--- public/project/config/routes.php
+++ public/project/config/routes.php
@@ -5,10 +5,6 @@ use Core\Route;
 return [
         
     new Route('/', 'index', 'hi'),
-    // new Route('/function/anonim_func/', 'function', 'anonim_func'),
-    // new Route('/function/external_var_anonim_func/', 'function', 'external_var_anonim_func'),
-    // new Route('/function/anonim_func_get_outer_var/', 'function', 'anonim_func_get_outer_var'),
     new Route('/json/', 'json', 'intro'),
-    // new Route('/function/:theme', 'function', 'intro'),
     new Route('/function/:theme', 'function', 'test'),
 ];
